fd8c10b29e2fb166db3a6304cc296ac5c95b6801
Kenny-exe <kujurken@gmail.com>
2026-06-02 17:48:08 +0530

    Implement complete user CRUD API

---
+++ src/main/java/com/recovertogether/backend/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.recovertogether.backend.controller;
+import java.util.List;
+import com.recovertogether.backend.entity.User;
+import com.recovertogether.backend.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //CREATE
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+
+        userRepository.save(user);
+
+        return "User Registered";
+    }
+
+    //READ
+    @GetMapping
+    public List<User> getAllUsers()
+    {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id)
+    {
+        return userRepository.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found"));
+    }
+
+    //DELETE
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id)
+    {
+        userRepository.deleteById(id);
+        return "User deleted successfully";
+    }
+
+    //UPDATE
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser)
+    {
+        User existingUser = userRepository.findById(id).orElseThrow(()->
+                new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found"));
+
+        existingUser.setName(updatedUser.getName());
+        existingUser.setEmail(updatedUser.getEmail());
+
+        return userRepository.save(existingUser);
+    }
+}
---
+++ src/main/java/com/recovertogether/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.recovertogether.backend.repository;
+
+import com.recovertogether.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
